a650550e9159ba162b41ab6b312ebc0dd7fbbed1
EduardoAyora <eduardoaayora64@gmail.com>
2022-11-14 10:54:32 -0500

    Agregar scroll a vista de pelicula individual

--- app/src/main/java/com/example/movies/MovieActivity.kt
+++ app/src/main/java/com/example/movies/MovieActivity.kt
@@ -3,13 +3,44 @@ package com.example.movies
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 class MovieActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
 
         val imdbID = intent.getStringExtra("imdbID")
-        Log.i("imdbID", imdbID + "")
+        if (imdbID != null) {
+            getMovieInformation(imdbID)
+        }
+    }
+
+    fun getMovieInformation(id: String) {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://www.omdbapi.com/?apikey=bcc3bc37&i=" + id
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                val mov = response.getString("Title")
+
+                Log.i("mov", mov)
+
+                val txtTitle: TextView = findViewById(R.id.txtTitleMovie)
+                txtTitle.text = mov
+
+            },
+            Response.ErrorListener { error ->
+                Log.e("Error", error.toString())
+            }
+        )
+        queue.add(jsonObjectRequest)
     }
 }
